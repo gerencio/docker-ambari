cc328c867501fe69c59bbfe9ea99423677beafd4
clayton@xdevel.com.br <clayton@xdevel.com.br>
2016-03-06 18:38:44 -0300

    change scripts for kylin services

--- ambari-server/install-cluster.sh
+++ ambari-server/install-cluster.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ./ambari-shell.sh << EOF
-blueprint add --url https://raw.githubusercontent.com/gerencio/docker-ambari/ambari-server/multi-node-hdfs-yarn.json
-blueprint add --url https://raw.githubusercontent.com/gerencio/docker-ambari/ambari-server/single-node-hdfs-yarn.json
+blueprint add --url https://raw.githubusercontent.com/gerencio/docker-ambari/2.2.0-kylin/ambari-server/blueprints/multi-node-hdfs-yarn.json
+blueprint add --url https://raw.githubusercontent.com/gerencio/docker-ambari/2.2.0-kylin/ambari-server/blueprints/single-node-hdfs-yarn.json
 cluster build --blueprint $BLUEPRINT
 cluster autoAssign
 cluster create --exitOnFinish true
